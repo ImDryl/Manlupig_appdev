dce50215466142df3b08ac84ce368f821a378066
Dryl <drielwanmanlupig@gmail.com>
2026-02-20 11:07:12 +0800

    feat: add login and register screens with nav

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,34 +1,16 @@
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-// import HomeScreen from '../screens/HomeScreen';
-// import ProfileScreen from '../screens/ProfileScreen';
+// Import the two Navigators we just separated
+import AuthNav from './AuthNav';
+import MainNav from './MainNav';
 
-// // screens
-// import HomeScreen from '../../android/app/src/screens/HomeScreen';
-// import ProfileScreen from '../../android/app/src/screens/ProfileScreen';
+export default function AppNav() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Change to true to test MainNav
 
-import HomeScreen from '../screens/HomeScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-
-// utils -> INDEX
-import { ROUTES } from '../utils';
-
-const Stack = createStackNavigator();
-
-const MainNavigation = () => {
-  return (
-    <Stack.Navigator initialRouteName={ROUTES.HOME}>
-      <Stack.Screen name={ROUTES.HOME} component={HomeScreen} />
-      <Stack.Screen name={ROUTES.PROFILE} component={ProfileScreen} />
-    </Stack.Navigator>
-  );
-};
-
-export default () => {
   return (
     <NavigationContainer>
-      <MainNavigation />
+      {isAuthenticated ? <MainNav /> : <AuthNav />}
     </NavigationContainer>
   );
-};
+}
